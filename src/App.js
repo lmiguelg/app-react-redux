6f46cb322818071e8f823c50a0260ba7090caf57
Miguel Gonçalves <miguel.goncalves@grupoacin.local>
2020-10-13 13:24:22 +0100

    first redux implementation on add courses

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route,Switch} from 'react-router-dom'
+import HomePage from './components/home/HomePage'
+import AboutPage from './components/about/AboutPage'
+import Header from './components/common/Header'
+import PageNotFound from './components/PageNotFound'
+import CoursesPage from './components/courses/CoursesPage'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container container-fluid">
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/courses" component={CoursesPage}/>
+        <Route component={PageNotFound}/>
+      </Switch>
+      
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './bootstrap.min.css';
+import configureStore from './redux/configureStore'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider as ReduxProvider } from 'react-redux'
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ReduxProvider store={store}>
+    <Router><App /></Router>
+  </ReduxProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
